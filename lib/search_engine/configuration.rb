8813fb85d4fc7bc171325b240461f8aaaec36143
Pratik <pganvir@digitsexperts.com>
2015-07-14 15:37:56 +0530

    Added comment into configuration module

--- lib/search_engine/configuration.rb
+++ lib/search_engine/configuration.rb
@@ -1,3 +1,6 @@
+# Configuration module holds all the configuration information of search engine.
+# column_name holds the name of the model column on which search needs to be performed.
+# limit holds the value of the maximum number of rows to be returned by the search result
 module SearchEngine
 	module Configuration
 		allowed_keys = [:model_name,:column_name,:limit]
